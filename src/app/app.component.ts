02d4d9ddd19a0d67c8a2c696fc2b20fd620a6eca
SamueleDellaRocca <samueledellarocca95@gmail.com>
2023-04-12 12:27:03 +0200

    funzione chiave-valore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'homeangular';
+
+
+  persone = [
+    { nome: "Mauro", cognome: "Rossi", isOnline: false, color: "green" },
+    { nome: "Rossella", cognome: "Brescia", isOnline: false, color: "blue" },
+    { nome: "Francesco", cognome: "Totti", isOnline: true, color: "yellow" },
+    { nome: "Daniele", cognome: "De Rossi", isOnline: true, color: "sky" },
+    { nome: "Paulo", cognome: "Dybala", isOnline: true, color: "orange" },
+  ]
+
+  indirizzi = ["indirizzo", "viale forza roma"]
+
+
+  constructor() {
+    this.pushaIndirizzi(this.persone, this.indirizzi);
+  }
+
+
+  pushaIndirizzi(arrOggetti: Array<any>, array: Array<string>) {
+    for (const oggetto of arrOggetti) {
+      oggetto[array[0]] = array[1]
+    }
+    console.log(arrOggetti)
+  }
+
+
 }
